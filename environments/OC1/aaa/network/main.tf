fee4058637c99ecea56d064a1e1ee80eb378174c
Ronaldo Chan <ronaldopchan@outlook.com>
2025-09-27 07:13:17 +0800

    Forgot to change CIDR

--- environments/OC1/aaa/network/main.tf
+++ environments/OC1/aaa/network/main.tf
@@ -2,5 +2,5 @@ module "network" {
   source         = "git::https://github.com/rpchan44/terraform-oci-vcn.git?ref=v0.0.1"
   compartment_id  = data.terraform_remote_state.compartment.outputs.compartment_id
   vcn_cidr        = "192.168.100.0/24"
-  subnet_cidr     = "192.168.100.0/28" 
+  subnet_cidr     = "192.168.100.0/27" 
 }
