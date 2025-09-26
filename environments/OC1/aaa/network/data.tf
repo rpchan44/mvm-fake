fd334e28c745142c9d7964bf2e37f0484fdf9049
Ronaldo Chan <ronaldopchan@outlook.com>
2025-09-27 07:10:13 +0800

    Add subnet_id output (#34)

--- environments/OC1/aaa/network/data.tf
+++ environments/OC1/aaa/network/data.tf
@@ -15,3 +15,21 @@ data "terraform_remote_state" "compartment" {
     }
   }
 }
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket                      = "terraform-states"
+    key                         = "project1/terraform.tfstate"
+    region                      = "us-east-1"
+    access_key                  = "minioadmin"
+    secret_key                  = "minioadmin"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style            = true
+    endpoints = {
+      s3 = "http://192.168.51.99:9000"
+    }
+  }
+}
