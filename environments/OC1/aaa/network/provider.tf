4554a57777e1d58ed2fac6e0e3ef62f07a357eb9
Ronaldo Chan <ronaldopchan@outlook.com>
2025-09-26 17:17:07 +0800

    Provide bucket for state

---
+++ environments/OC1/aaa/network/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket                      = "terraform-states"
+    key                         = "project1/terraform.tfstate"
+    region                      = "us-east-1"
+    access_key                  = "minioadmin"
+    secret_key                  = "minioadmin"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+    endpoints = {
+      s3 = "http://192.168.51.99:9000"
+    }
+  }
+}
+
